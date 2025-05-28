6e5049ad29fe6ac2cbc21aecf9f60076a7e4e809
AlexisEgea <alexisegea@outlook.com>
2025-05-29 01:25:07 +0200

    init project

---
+++ src/form.js
@@ -0,0 +1,10 @@
+import * as THREE from 'three';
+
+export function createSimpleShape() {
+  // Creation of a cube
+  const geometry = new THREE.BoxGeometry(1, 1, 1);
+  const material = new THREE.MeshStandardMaterial({ color: 0xffffff, metalness: 0.3, roughness: 0.4 });
+  const mesh = new THREE.Mesh(geometry, material);
+  mesh.position.set(0, 0, 0);
+  return mesh;
+}
